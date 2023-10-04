13db2c6f19cf2523d1199261142bc3af5d7656d2
DhyanShah22 <dhyanshah229@gmail.com>
2023-10-04 17:31:49 +0530

    Part 2

---
+++ Variables/ShortVarDec/short-vardec.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i, j int = 1, 2
+	k := 3  //short declarations
+	var c, python, java = true, false, true
+	fmt.Println(i, j, k)
+	fmt.Println(c, python, java)
+}
